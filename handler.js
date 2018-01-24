00a1a51575fd480f6b01af2410baad111f264e78
Shyam Bhat <shyambhat@me.com>
2018-01-24 14:41:10 +0100

    second commit

---
+++ handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      expose: event.pathParameters.exposeId,
+    }),
+  };
+
+  callback(null, response);
+
+
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
